f2949864a4d594255969a3dcf4d44a293a39b8be
johnabass <johnbass1017@yahoo.com>
2024-11-08 17:01:18 -0800

    refactored into a proper service locator abstraction

--- names.go
+++ names.go
@@ -10,107 +10,118 @@ func normalizeName(v string) string {
 	return strings.ToLower(v)
 }
 
-// emptyNames is a canonical empty Names set.
-var emptyNames *Names = new(Names)
-
-// Names is a set of normalized, unique names for hashing.
-// A Names instance is immutable once created.
-//
-// The zero value of this type is an empty Names set.  Use
-// NewNames to create a non-empty set.
+// Names is an immutable set of service names.  Names are deduped
+// and normalized. The zero value of this type is an empty set.
+// Use NewNames to create a non-empty Names. A nil Names is valid,
+// and is treated as empty by its methods.
 type Names struct {
 	n map[string]bool
 }
 
-// NewNames constructs an immutable Names set from the given
-// slice of names.  Names are normalized, and duplicate names
-// are ignored.
-func NewNames(names ...string) *Names {
-	if len(names) == 0 {
-		return emptyNames
+// NewNames constructs an immutable Names set.
+func NewNames(values ...string) *Names {
+	names := &Names{
+		n: make(map[string]bool, len(values)),
 	}
 
-	n := &Names{
-		n: make(map[string]bool, len(names)),
+	for _, v := range values {
+		names.n[normalizeName(v)] = true
 	}
 
-	for _, name := range names {
-		n.n[normalizeName(name)] = true
-	}
-
-	return n
+	return names
 }
 
-// Len returns the count of names in this set.
+// Len returns the count of service names in this set.
+// If this Names is nil, this method returns zero (0).
 func (n *Names) Len() int {
+	if n == nil {
+		return 0
+	}
+
 	return len(n.n)
 }
 
-// Has tests if this Names set has the given name.
+// Has tests if this set contains the normalized version of a given service name.
+// If this Names is nil, this method always returns false.
 func (n *Names) Has(v string) bool {
-	_, exists := n.n[normalizeName(v)]
-	return exists
-}
-
-// Each visits each name in this set. The predicate can return
-// false to exit early. The order in which names are visited is undefined.
-func (n *Names) Each(f func(string) bool) {
-	for name := range n.n {
-		if !f(name) {
-			return
-		}
-	}
-}
-
-// Merge produces the union of this Names instance with another set.
-func (n *Names) Merge(more *Names) *Names {
-	switch {
-	case more.Len() == 0:
-		return n
-
-	case n.Len() == 0:
-		return more
-
-	default:
-		mergedMap := make(map[string]bool, n.Len()+more.Len())
-		for name := range n.Each {
-			mergedMap[name] = true
-		}
-
-		for name := range more.Each {
-			mergedMap[name] = true
-		}
-
-		return &Names{n: mergedMap}
-	}
-}
-
-// Update compares a possibly distinct list of names with this Names.
-// If the newNames list is the same, accounting for duplicates, this
-// method returns this existing Names instance along with false.
-// If the newNames list is different, a new Names instance is returned
-// along with true.
-func (n *Names) Update(list ...string) (*Names, bool) {
-	if len(list) == 0 {
-		return emptyNames, len(n.n) != 0
+	if n == nil {
+		return false
 	}
 
-	// have to account for any duplicates in the updated list
-	updatedMap := make(map[string]bool, len(list))
-	newName := false
-	for _, name := range list {
-		name = normalizeName(name)
-		updatedMap[name] = true
-		if !newName {
-			if _, exists := n.n[name]; !exists {
-				newName = true
+	_, has := n.n[normalizeName(v)]
+	return has
+}
+
+// All provides iteration over each name in this set.
+// If this Names is nil, this method does nothing.
+func (n *Names) All(f func(string) bool) {
+	if n != nil {
+		for name := range n.n {
+			if !f(name) {
+				return
 			}
 		}
 	}
+}
 
-	if newName || len(n.n) != len(updatedMap) {
-		return &Names{n: updatedMap}, true
+// Merge returns a Names set that is the union of this Names
+// with the given list. If list is empty, this method returns
+// this Names as is. If this Names is nil or has zero length,
+// NewNames is used to create a new Names with the given list.
+func (n *Names) Merge(list ...string) *Names {
+	switch {
+	case len(list) == 0:
+		return n
+
+	case n == nil || n.Len() == 0:
+		return NewNames(list...)
+
+	default:
+		merged := make(map[string]bool, n.Len()+len(list))
+		for name := range n.n {
+			merged[name] = true
+		}
+
+		for _, name := range list {
+			merged[normalizeName(name)] = true
+		}
+
+		return &Names{n: merged}
 	}
+}
 
-	return n, false
+// Update tests if a list of names would represent an update to
+// this set of names. If list represents a different set of names,
+// accounting for duplicates, this method returns a distinct names
+// instance and true. Otherwise, this method returns this names
+// and false.
+//
+// Names are always immutable. This method does not modify the
+// original Names set.
+func (n *Names) Update(list ...string) (*Names, bool) {
+	switch {
+	case len(list) == 0:
+		return n, false // no update
+
+	case n == nil || n.Len() == 0:
+		return NewNames(list...), true // the entire list is the update
+
+	default:
+		// have to account for any duplicates in the updated list
+		updated := make(map[string]bool, len(list))
+		sameNames := true
+		for _, name := range list {
+			name = normalizeName(name)
+			updated[name] = true
+			if sameNames {
+				_, sameNames = n.n[name]
+			}
+		}
+
+		if !sameNames || n.Len() != len(updated) {
+			return &Names{n: updated}, true
+		}
+
+		return n, false
+	}
 }
--- ring.go
+++ ring.go
@@ -2,97 +2,45 @@ package hashy
 
 import (
 	"bytes"
+	"hash"
 	"sort"
 	"strconv"
-	"unsafe"
 )
-
-const (
-	DefaultRingVNodes int = 200
-)
-
-type RingOption interface {
-	apply(*Ring) error
-}
-
-type ringOptionFunc func(*Ring) error
-
-func (f ringOptionFunc) apply(r *Ring) error { return f(r) }
-
-// WithHash sets the Hash algorithm used by the Ring. By default,
-// Murmur3 is used.
-func WithHash(h Hash) RingOption {
-	return ringOptionFunc(func(r *Ring) error {
-		r.hash = h
-		return nil
-	})
-}
-
-// WithVNodes sets the number of replica nodes within the ring.
-// The default is DefaultRingVNodes.
-func WithVNodes(v int) RingOption {
-	return ringOptionFunc(func(r *Ring) error {
-		r.vnodes = v
-		return nil
-	})
-}
-
-// WithNames adds names to this ring.  Each invocation of this
-// option will produce a merged, deduped set of names to occupy
-// the ring.
-func WithNames(n *Names) RingOption {
-	return ringOptionFunc(func(r *Ring) error {
-		r.names = r.names.Merge(n)
-		return nil
-	})
-}
 
 type ringNode struct {
 	token uint64
 	name  string
 }
 
-type ringNodes []ringNode
+type ring []ringNode
 
-func (rn ringNodes) Len() int {
-	return len(rn)
+func (r ring) Len() int {
+	return len(r)
 }
 
-func (rn ringNodes) Less(i, j int) bool {
-	return rn[i].token < rn[j].token
+func (r ring) Less(i, j int) bool {
+	return r[i].token < r[j].token
 }
 
-func (rn ringNodes) Swap(i, j int) {
-	rn[i], rn[j] = rn[j], rn[i]
+func (r ring) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
 }
 
-// Ring provides a hash ring for names.  A Ring is immutable once
-// created. This implementation follows github.com/billhathaway/consistentHash
-// for backwards compatibility with xmidt.
-type Ring struct {
-	vnodes int
-	hash   Hash
-	names  *Names
-	nodes  ringNodes
-}
-
-// initialize builds the ringNodes for this Ring.
-func (r *Ring) initialize() {
+func newRing(vnodes int, hash hash.Hash64, names *Names) (r ring) {
 	var (
-		hash       = r.hash.New64()
 		nameBuffer bytes.Buffer
 		prefix     = make([]byte, 0, 32) // grab enough that reallocations are unlikely
 	)
 
 	// preallocate as much as possible
-	r.nodes = make(ringNodes, 0, r.names.Len()*r.vnodes)
+	r = make(ring, 0, names.Len()*vnodes)
 	nameBuffer.Grow(256)
 
-	for name := range r.names.Each {
+	for name := range names.All {
 		nameBuffer.Reset()
 		nameBuffer.WriteString(name) // convert name to bytes only once
 
-		for increment := int64(0); increment < int64(r.vnodes); increment++ {
+		for increment := int64(0); increment < int64(vnodes); increment++ {
 			// this keeps the same format as github.com/billhathaway/consistentHash:
 			// {integer}={name}
 			hash.Reset()
@@ -101,90 +49,30 @@ func (r *Ring) initialize() {
 			hash.Write(prefix)
 			hash.Write(nameBuffer.Bytes())
 
-			r.nodes = append(r.nodes,
-				ringNode{
-					token: hash.Sum64(),
-					name:  name,
-				},
-			)
+			r = append(r, ringNode{
+				token: hash.Sum64(),
+				name:  name,
+			})
 		}
 	}
 
-	sort.Sort(r.nodes)
+	sort.Sort(r)
+	return
 }
 
-// Update checks to see if the given set of names is different from the one
-// currently hashed. If there are no differences, this Ring is returned as is
-// along with false. If a new Ring had to be created, that new Ring is returned
-// along with true.
-//
-// The returned Ring will always have the same configuration, e.g. vnodes, as
-// this Ring.
-func (r *Ring) Update(list ...string) (*Ring, bool) {
-	if updatedNames, updated := r.names.Update(list...); updated {
-		updatedRing := &Ring{
-			hash:   r.hash,
-			vnodes: r.vnodes,
-			names:  updatedNames,
-		}
-
-		updatedRing.initialize()
-		return updatedRing, true
-	}
-
-	return r, false
-}
-
-// Get is like GetBytes, but avoids unnecessary allocation when
-// hashing a string.
-func (r *Ring) Get(v string) string {
-	if len(v) == 0 {
-		return ""
-	}
-
-	return r.GetBytes(
-		unsafe.Slice(unsafe.StringData(v), len(v)),
-	)
-}
-
-// GetBytes hashes the given value and returns the closest address on the ring.
-func (r *Ring) GetBytes(v []byte) string {
-	if r.nodes.Len() == 0 {
-		return ""
-	}
-
-	target := r.hash.Sum64(v)
+// get returns the name on the ring whose token is the closest
+// to the given target value.
+func (r ring) get(target uint64) string {
 	index := sort.Search(
-		r.nodes.Len(),
+		r.Len(),
 		func(i int) bool {
-			return r.nodes[i].token >= target
+			return r[i].token >= target
 		},
 	)
 
-	if index >= r.nodes.Len() {
+	if index >= r.Len() {
 		index = 0
 	}
 
-	return r.nodes[index].name
-}
-
-// NewRing produces a hash ring with the given options.
-func NewRing(opts ...RingOption) (*Ring, error) {
-	r := new(Ring)
-	for _, o := range opts {
-		if err := o.apply(r); err != nil {
-			return nil, err
-		}
-	}
-
-	if r.hash == nil {
-		r.hash = Murmur3{}
-	}
-
-	if r.vnodes <= 0 {
-		r.vnodes = DefaultRingVNodes
-	}
-
-	r.initialize()
-	return r, nil
+	return r[index].name
 }
