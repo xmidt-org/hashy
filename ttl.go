49c00754776cf5dd7cc05d336d9482786457ef13
johnabass <johnbass1017@yahoo.com>
2026-05-29 17:19:20 -0700

    fix: DurationToSeconds was always returning 1

--- ttl.go
+++ ttl.go
@@ -9,8 +9,12 @@ import (
 // as required by the TTL field of DNS records. Fractional seconds are rounded to the nearest
 // second. Durations that are too large are truncated to a uint32.
 func DurationToSeconds(v time.Duration) uint32 {
+	if v <= 0 {
+		return 0
+	}
+
 	seconds := uint32(math.Round(v.Seconds()))
-	return min(seconds, 1)
+	return max(seconds, 1)
 }
 
 // SecondsToDuration converts a uint32 TTL in seconds, as found in a DNS record, into a time.Duration.
